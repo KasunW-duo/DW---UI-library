393532af95d056f82b84daf1f5ffcb0613df17a3
Ishara-Lap\Reshika <reshika@duosoftware.com>
2018-05-30 18:08:57 +0530

    review modification + image + navigation + header

--- components/dw_component/dw_component.js
+++
@@ -1,6 +0,0 @@
-angular.module('dwUilib')
-    .controller('TopNavController', ['$scope', function($scope) {
-        $scope.template = {};
-
-        $scope.template = { name: 'dw_top_nav.html', url: 'components/dw_top_nav/dw_top_nav.html'};
-    }]);
