aab97c1af1b31cdd77e2748a6441f3ffb8e4bf40
KasunW-duo <kasun.w@duosoftware.com>
2018-07-04 12:03:36 +0530

    Code preview integration

---
+++ components/dw_main.controller.js
@@ -0,0 +1,17 @@
+angular.module('dwUilib')
+    .controller('MainController', ['$scope', '$http', '$state', function ($scope, $http, $state) {
+
+        $scope.data = {};
+        $scope.state = $state;
+
+        $http.get('components/components.json').then(function (response) {
+            $scope.data = response.data;
+        });
+
+        $scope.getCode = function(state, id) {
+            $http.get('components/' + id + '/' + id + '.html').then(function (response) {
+                $('#dwCodePreview').val(response.data);
+                $state.go(state);
+            });
+        };
+}]);
